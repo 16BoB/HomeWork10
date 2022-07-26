57f9cd04cf648803e0cb50e4b3e95c9be9ef04f4
Tape <trid.skivi@gmail.com>
2022-07-26 23:20:16 +0300

    write methods for print array to console

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
 // Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
 
+string[] CreateStringArr (int n) {
+    string[] arr = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите элемент массива строк номер: " + i + 1);
+        string newElem = Convert.ToString(Console.ReadLine());
+        arr[i] = newElem;
+    }
+    return arr;
+}
+
+void PrintArr (string[] arr) {
+    int len = arr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
