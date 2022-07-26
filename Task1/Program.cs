5a5b734f938b1cbee704bc5ddeae846477c886fe
Tape <trid.skivi@gmail.com>
2022-07-26 23:43:09 +0300

    write code for first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,21 +1,48 @@
 ﻿// Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
 // Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
 
-string[] CreateStringArr (int n) {
+string[] CreateStringArr(int n)
+{
     string[] arr = new string[n];
     for (int i = 0; i < n; i++)
     {
-        Console.Write("Введите элемент массива строк номер: " + i + 1);
+        Console.Write("Введите элемент массива строк номер: " + (i + 1) + " ");
         string newElem = Convert.ToString(Console.ReadLine());
         arr[i] = newElem;
     }
     return arr;
 }
 
-void PrintArr (string[] arr) {
+void PrintArr(string[] arr)
+{
     int len = arr.Length;
     for (int i = 0; i < len; i++)
     {
-        Console.WriteLine(arr[i]);
+        Console.Write(arr[i] + ";");
     }
+    Console.WriteLine();
 }
+
+int CountWords(string[] arr)
+{
+    int len = arr.Length;
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i][0] == vowels[0] || arr[i][0] == vowels[1] || arr[i][0] == vowels[2] || arr[i][0] == vowels[3] || arr[i][0] == vowels[4] || arr[i][0] == vowels[5])
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите количество слов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] userArr = CreateStringArr(n);
+
+PrintArr(userArr);
+
+Console.WriteLine("Количество слов на гласную букву: " + CountWords(userArr));
