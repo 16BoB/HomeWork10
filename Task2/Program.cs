386b0db186456975911235a36310abf32dc0cc17
Tape <trid.skivi@gmail.com>
2022-07-26 23:49:15 +0300

    method for create array

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
 // Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
+
+string[] CreateStringArr(int n)
+{
+    string[] arr = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите элемент массива строк номер: " + (i + 1) + " ");
+        string? newElem = Convert.ToString(Console.ReadLine());
+        arr[i] = newElem;
+    }
+    return arr;
+}
